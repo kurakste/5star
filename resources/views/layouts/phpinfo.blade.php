fdb3c76ddd9c5bd19f8cd41c998abdbe5acadf6b
Stepan <kurakste@gmail.com>
2018-01-16 21:57:10 +0300

    add phpinfo.blade.php

---
+++ resources/views/layouts/phpinfo.blade.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kurakste
+ * Date: 16.01.2018
+ * Time: 21:54
+ */
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ phpinfo()}}
+                            </div>
+                        @endif
+
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
