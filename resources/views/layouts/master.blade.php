a1fc0829b9c6cebdc964b2ec508fe3b55b0b675d
Stepan <kurakste@gmail.com>
2018-01-16 23:37:17 +0300

    update url for QR

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                  <a class="nav-link" href="/main">Главная<span class="sr-only"></span></a>
+                  <a class="nav-link" href="/home">Главная<span class="sr-only"></span></a>
               </li>
               <li class="nav-item">
                   <a class="nav-link" href="/object/add">Добавить объект </a>
