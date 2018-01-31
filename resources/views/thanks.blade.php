f0eb0a74f2f43f4ba5330e4390f293a3bcb99be1
Stepan <kurakste@gmail.com>
2018-01-31 10:09:58 +0300

    small changes

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -1,7 +1,23 @@
 
-@extends('layouts.master')
+@extends('layouts.fbmaster')
 @section('title', '5StarService')
 
 @section('content')
-<h1> Thanks a lot!!! </h1>
+    <style>
+        .wrapper {
+            height: 100%;
+            width: 100%;
+        }
+        h1 {
+            font-size: 5vw;
+            text-transform: uppercase;
+            margin-top: 35%;
+            margin-lef:10%;
+            margin-right:10%;
+            text-align: center;
+        }
+    </style>
+    <div class="wrapper">
+        <h1> Огромное спасибо за вашу помощь </h1>
+    </div>
 @endsection
