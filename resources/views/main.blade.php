1da05ac67103475b99851d1d9c321c1df830910c
Stepan <kurakste@gmail.com>
2018-02-01 20:40:28 +0300

    small changes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,7 +12,7 @@
         font-weight: bold;
         position: relative;
         margin: 5px;
-        height: 38vh;
+        height: 34vh;
         text-transform: uppercase;
         color: #27282d;
         background-color: #fbfbfb;
