ce4eb96e925108b511f5ef305933af5739b6ae08
Stepan <kurakste@gmail.com>
2018-02-04 10:18:41 +0300

    small changes

--- resources/views/addobject.blade.php
+++ resources/views/addobject.blade.php
@@ -52,7 +52,7 @@
     <div class="container-fluid">
 
         <div class="addModule">
-            <div><a href="/home" class="btn btn-outline-success float-btn"><i class="fas fa-angle-left"></i></a></div>
+            <div><a href="/objects" class="btn btn-outline-success float-btn"><i class="fas fa-angle-left"></i></a></div>
             <div><a href="/object/add" class="btn btn-outline-success float-btn"><i class="fas fa-plus-circle"></i></a></div>
             <button type="submit" form ="mainForm" class="btn btn-outline-success float-btn"><i class="far fa-save"></i></button>
 
