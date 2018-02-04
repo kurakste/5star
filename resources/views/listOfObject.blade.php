0ac93d68ebab93103d9fca92b9ba36a75067c5b3
Stepan <kurakste@gmail.com>
2018-02-04 08:42:36 +0300

    small changes

--- resources/views/listOfObject.blade.php
+++ resources/views/listOfObject.blade.php
@@ -67,8 +67,15 @@
         right : 10px;
         z-index: 100;
     }
+    
+    .float-btn {
+        margin:5px;
+        width: 50px;
+    } 
 
-    .btn_cl, .btn_obj {margin:2px;}
+
+    .btn_obj {margin:2px;}
+
 </style>
 
 
@@ -78,7 +85,9 @@
 @section('content')
 <div class="container-fluid">
     <div class="addModule">
-        <a href="object/add" class="btn btn-outline-success"><i class="fas fa-plus-circle"></i></a>
+        <div><a href="home" class="btn btn-outline-success float-btn"><i class="fas fa-angle-left"></i></a></div>
+        <div><a href="object/add" class="btn btn-outline-success float-btn"><i class="fas fa-plus-circle"></i></a></div>
+
 
     </div>
 
@@ -155,40 +164,6 @@
 @endsection
 
 @section('script')
-    <script>
 
-        (function( win ){
-            var doc = win.document;
-            // If there's a hash, or addEventListener is undefined, stop here
-            if(!win.navigator.standalone && !location.hash && win.addEventListener ){
-
-                //scroll to 1
-                win.scrollTo( 0, 1 );
-                var scrollTop = 1,
-                    getScrollTop = function(){
-                        return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-                    },
-
-                    //reset to 0 on bodyready, if needed
-                    bodycheck = setInterval(function(){
-                        if( doc.body ){
-                            clearInterval( bodycheck );
-                            scrollTop = getScrollTop();
-                            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-                        }
-                    }, 15 );
-
-                win.addEventListener( "load", function(){
-                    setTimeout(function(){
-                        //at load, if user hasn't scrolled more than 20 or so...
-                        if( getScrollTop() < 20 ){
-                            //reset to hide addr bar at onload
-                            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-                        }
-                    }, 0);
-                }, false );
-            }
-        })( this );
-    </script>
 
 @endsection
