9b688de68a88904cb192bede1c00b20209453448
Stepan <kurakste@gmail.com>
2018-02-10 21:38:09 +0300

    small changes after migration on matireall

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -50,9 +50,39 @@
         <div class = "mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet
                                              mdl-cell--2-col-phone graybox">
 
+            <div class="demo-card-square mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h6 class="mdl-card__title-text">НАСТРОЙКИ</h6>
+              </div>
+              <div class="mdl-card__supporting-text">
+                Вся информация о ваших настройках.  
+              </div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a href="/info" 
+                   class="mdl-button mdl-button--colored 
+                          mdl-js-button mdl-js-ripple-effect">
+                  ВПЕРЕД
+                </a>
+              </div>
+            </div>
         </div>
        <div class = "mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet 
                                                 mdl-cell--2-col-phone graybox">
+            <div class="demo-card-square mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h6 class="mdl-card__title-text">НАСТРОЙКИ</h6>
+              </div>
+              <div class="mdl-card__supporting-text">
+                Вся информация о ваших настройках.  
+              </div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a href="/info" 
+                   class="mdl-button mdl-button--colored 
+                          mdl-js-button mdl-js-ripple-effect">
+                  ВПЕРЕД
+                </a>
+              </div>
+            </div>
         </div>
     </div>
          
