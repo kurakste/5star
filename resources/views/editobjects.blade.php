1eceb46d4a8319656a7daa470d63619701687a90
Stepan <kurakste@gmail.com>
2018-02-11 20:51:17 +0300

    small changes after migration on matireall

--- resources/views/editobjects.blade.php
+++ resources/views/editobjects.blade.php
@@ -2,104 +2,59 @@
 @section('title', '  HotLine')
 @section('content')
 
-    <style type="text/css">
-    .container-fluid {
+    <button type="submit" form ="mainForm" 
+            class="mdl-button mdl-js-button mdl-button--fab" id="editObjectButton">
+      <a href="/object/add"><i class="material-icons">save</i></a> 
+    </button>
 
-        height: 800px;
-    }
-    .wrapper  {
-            font-size: 0.7rem;
-            font-family: 'Montserrat', sans-serif;
-            font-weight: bold;
-            position: relative;
-            margin: 5px;
-            height: 750px;
-            text-transform: uppercase;
-            color: #27282d;
-            background-color: #fbfbfb;
-        }
+    <button  
+            class="mdl-button mdl-js-button mdl-button--fab" id="backButton">
+     <a href="/objects"><i class="material-icons">keyboard_arrow_left</i></a> 
 
-        .border {
-            padding-top: 2vh;
-            padding-left: 2vh;
-            padding-right: 2vh;
-            position: absolute;
-            top:1.7vh;
-            left: 2vw;
-            border: solid #aa9e72 1px;
-            height: 730px;
-            width:95%;
-        }
+    </button>
 
-    .addModule {
-        position: absolute;
-        top: 20px;
-        right : 10px;
-        z-index: 100;
-    }
+    <div class="mdl-card mdl-shadow--2dp editObjectCard">
+      <div class="mdl-card__title mdl-card--expand">
+        <h2 class="mdl-card__title-text">{{$object->nick}}</h2>
+      </div>
+      <div class="mdl-card__supporting-text">
 
-    .float-btn {
-        margin:5px;
-        width: 50px;
-    }
-
-        @media (orientation: landscape) {
-
-        }
-
-    </style>
-    <div class="container-fluid">
-    <!--<div class="row" id='clientData'> -->
-        <div class="addModule">
-            <div><a href="/objects" class="btn btn-outline-success float-btn"><i class="fas fa-angle-left"></i></a></div>
-            <button type="submit" form ="mainForm" class="btn btn-outline-success float-btn"><i class="far fa-save"></i></button>
-        </div>
-
-
-    <div class="wrapper">
-        <div class="border">
             <form action="/object/store" id="mainForm" method="post">
                 {{ csrf_field() }}
 
                 <input type="hidden" name="id"  value="{{$object->id}}">
                 <input type="hidden" name="fuser_id"  value="{{$object->user_id}}">
 
-                <div class="form-group">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                     <label for="FNickname">Никнейм:</label>
-                    <input type="text" name="fnickname" value="{{$object->nick}}"  class="form-control" id="FNickname" aria-describedby="HNickname" placeholder="Введите никнейм вашего объекта." required pattern="^[A-Za-z0-9]+$">
-                    <small id="HNickname" class="form-text text-muted text-justify">Никнеймы объекта будут помогать вам найти нужный объект среди ваших объектов. Он должен быть уникальным, если никнейм который вы вводите уже существует то мы предложим создать новый. Он должен состоять из цифр и букв латинского алфавита.</small>
+                    <input type="text" name="fnickname" value="{{$object->nick}}"  
+                    class="mdl-textfield__input" id="FNickname" aria-describedby="HNickname" 
+                    placeholder="Введите никнейм вашего объекта." 
+                    required pattern="^[A-Za-z0-9]+$">
                 </div>
-                <div class="form-group">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                     <label for="FNotes">Город:</label>
-                    <input type="text" class="form-control" name="fcity" value="{{$object->city}}" id="FCity" aria-describedby="HNotes" placeholder="Введите город." pattern="^[0-9a-zA-Zа-яА-Я\s.,:!?-]+$">
-                    <small id="HNotes" class="form-text text-muted">Введите город.</small>
+                    <input type="text" class="mdl-textfield__input form-control" name="fcity" value="{{$object->city}}" id="FCity" aria-describedby="HNotes" placeholder="Введите город." pattern="^[0-9a-zA-Zа-яА-Я\s.,:!?-]+$">
                 </div>
-                <div class="form-group">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                     <label for="FAddr">Адрес:</label>
-                    <input type="text" class="form-control" name="faddr" value="{{$object->addr}}" id="FAddr" aria-describedby="HAddr" placeholder="Введите адрес объекта." pattern="^[0-9a-zA-Zа-яА-Я\s.,:;!?-]+$">
-                    <small id="HNotes" class="form-text text-muted">Введите  адрес.</small>
+                    <input type="text" class="mdl-textfield__input form-control" name="faddr" value="{{$object->addr}}" id="FAddr" aria-describedby="HAddr" placeholder="Введите адрес объекта." pattern="^[0-9a-zA-Zа-яА-Я\s.,:;!?-]+$">
                 </div>
-                <div class="form-group">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                     <label for="FManager">Менеджер:</label>
-                    <input type="text" name="fmanager" value="{{$object->managername}}"class="form-control" id="FManager" aria-describedby="HManager" placeholder="Введите имя менеджера." pattern="^[A-Za-z0-9а-яА-Я\s]+$">
-                    <small id="HManager" class="form-text text-muted">Введите имя менеджера ответственного за объект:</small>
+                    <input type="text" name="fmanager" value="{{$object->managername}}"class="mdl-textfield__input form-control" id="FManager" aria-describedby="HManager" placeholder="Введите имя менеджера." pattern="^[A-Za-z0-9а-яА-Я\s]+$">
                 </div>
-                <div class="form-group">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                     <label for="fphone">Телефон:</label>
-                    <input type="text" name="fphone" value="{{$object->managerphone}}" class="form-control" id="FPhone" aria-describedby="HPhone" placeholder="Введите номер телефона менеджера." pattern="^[0-9]{10}$">
-                    <small id="HPhone" class="form-text text-muted">Введите номер телефона менеджера ответственного за объект. Тербуется ввести 10-ть цифр номера. Например: 9869347745</small>
+                    <input type="text" name="fphone" value="{{$object->managerphone}}" class="mdl-textfield__input form-control" id="FPhone" aria-describedby="HPhone" placeholder="Введите номер телефона менеджера." pattern="^[0-9]{10}$">
                 </div>
-                <div class="form-group">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                     <label for="FNotes">Примечания:</label>
-                    <input type="text" class="form-control" name="fnotes" value="{{$object->notes}}" id="FNotes" aria-describedby="HNotes" placeholder="Любые примечания." pattern="^[a-zA-Zа-яА-Я\s.,:;!?-+]+$">
-
-                    <small id="HNotes" class="form-text text-muted">Здесь можно ввести любые примечания для вашего объекта.</small>
+                    <input type="text" class="mdl-textfield__input form-control" name="fnotes" value="{{$object->notes}}" id="FNotes" aria-describedby="HNotes" placeholder="Любые примечания." pattern="^[a-zA-Zа-яА-Я\s.,:;!?-+]+$">
                 </div>
             </form>
-        </div>
     </div>
-
-    </div> <!--container-fluid -->
+    </div>
 
 
 
