9f40e0c1407187ec615fb8b34699056abe11d3cf
Stepan <kurakste@gmail.com>
2018-02-15 15:53:59 +0300

    small changes after migration on matireall

--- resources/views/posterList.blade.php
+++ resources/views/posterList.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.master')
-@section('title', '5StarService')
+@section('title', 'HotLine')
 @section('content')
 
+<div class="add-wrapper">
+    
+@foreach ($postersList as $poster)
 
-        @foreach ($postersList as $poster)
-            <p><a href="getposter?poster={{$poster}}&id={{$object_id}}"><img src="{{$poster}}" alt="" width="300px" style="margin: 5px"></a></p>
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title mdl-card--expand">
+  </div>
+  <div class="mdl-card__supporting-text">
+    <img class='post-prev' src="{{$poster}}" alt=""> 
+    <p></p>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" 
+           href="getposter?poster={{$poster}}&id={{$object_id}}"
+        >
+        Скачать
+    </a>
+</div>
+</div>
 
-        @endforeach
+@endforeach
 
 @endsection
+</div>
