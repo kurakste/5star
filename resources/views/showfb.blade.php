6e0997f0cd2af4fa57d9c14c14a65119bad07deb
Stepan <kurakste@gmail.com>
2018-02-24 18:53:05 +0300

    small changes after migration on matireall

--- resources/views/showfb.blade.php
+++ resources/views/showfb.blade.php
@@ -3,12 +3,8 @@
 @section('content')
 
     <a class='bBack' href="/objects"><img src="/icon/left-80.png" alt="" width='60' /></a>
-
-    <div class="buffer"> </div>
-
     @foreach ($fbarray as $fb)
-    <div class="mdl-card mdl-shadow--2dp show-fb-card"> 
-      <div class="mdl-card__supporting-text show-fb-support">
+      <div class="show-fb-card">
         <div class="wrapper">
                 <ul class ='fbLeft'>
                   <li class = 'rw'>дата</li>
@@ -29,14 +25,8 @@
             <p class='p-fb'>Комментарий:</p>
             <p  class='p-fb'>{{$fb['comment']}}</p>
          </div> <!-- wrapper -->
-<!--        <div class="clearBlock"></div>
-        <div class="comment">
-            <p>Комментарий:</p>
-            <p>{{$fb['comment']}}</p>
-        </div>
--->
+
     </div> <!-- supporting-text -->
-  </div> <!--show-fb-card -->
   @endforeach
  @endsection
 
