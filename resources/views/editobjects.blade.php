5aebebdff55af445a4f602d82b490de63c2c71d4
Stepan <kurakste@gmail.com>
2018-02-26 07:00:18 +0300

    small changes after migration on matireall

--- resources/views/editobjects.blade.php
+++ resources/views/editobjects.blade.php
@@ -13,7 +13,7 @@
 
     <div class="buffer"> </div>
     <div class="mdl-card mdl-shadow--2dp editObjectCard">
-      <div class="mdl-card__title mdl-card--expand">
+      <div class="mdl-card__title mdl-card--expand object-title">
         <h2 class="mdl-card__title-text">{{$object->nick}}</h2>
       </div>
       <div class="mdl-card__supporting-text">
